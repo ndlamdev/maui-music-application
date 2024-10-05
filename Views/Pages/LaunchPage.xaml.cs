d7022394ecff36454ae8e5a7b66f69a68c43c73b
Nguyễn Đình Lam <21130416@st.hcmuaf.edu.vn>
2024-10-06 00:07:41 +0700

    feat: Hoàn thành trang Launch

---
+++ Views/Pages/LaunchPage.xaml.cs
@@ -0,0 +1,21 @@
+// Author: Nguyen Dinh Lam
+// Email: kiminonawa1305@gmail.com
+// Phone number: +84 855354919
+// Create at: 22:10:39 - 05/10/2024
+// User: Lam Nguyen
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maui_music_application.Views.Pages;
+
+public partial class LaunchPage : ContentPage
+{
+    public LaunchPage()
+    {
+        InitializeComponent();
+    }
+}
