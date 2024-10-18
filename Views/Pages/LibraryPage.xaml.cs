9539e2e6b6b5e208b6073331c3d5825e2914d775
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2024-10-18 23:26:02 +0700

    feat: create layout page Library

--- Views/Pages/LibraryPage.xaml.cs
+++ Views/Pages/LibraryPage.xaml.cs
@@ -4,6 +4,8 @@
 // Create at: 09:09:07 - 21/09/2024
 // User: Lam Nguyen
 
+using maui_music_application.Views.Adapters;
+
 namespace maui_music_application.Views.Pages;
 
 public partial class LibraryPage
@@ -11,5 +13,15 @@ public partial class LibraryPage
     public LibraryPage()
     {
         InitializeComponent();
+        Init();
+    }
+
+    private void Init()
+    {
+        RecentPlaylist.Adapter(new PlayListMusicLargeAdapter(DataDemo.PlayListLarges));
+    }
+
+    private void Search_OnTapped(object? sender, TappedEventArgs e)
+    {
     }
 }
